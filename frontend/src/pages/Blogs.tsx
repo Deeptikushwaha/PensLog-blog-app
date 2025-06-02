d12e518f18f7ce666fab3d0d2a27abb01c9064bd
Deeptikushwaha <deeptikushwaha140@gmail.com>
2025-06-03 02:01:34 +0530

    fixed some bugs

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,35 +1,35 @@
 import { Appbar } from "../components/Appbar";
-import {Blogcard} from "../components/blogcard";
-import {useBlogs} from "../hooks";
-import {BlogSkeleton} from "../components/BlogSkeleton";
+import { Blogcard } from "../components/Blogcard";
+import { useBlogs } from "../hooks";
+import { BlogSkeleton } from "../components/BlogSkeleton";
 
 export const Blogs = () => {
-   const {loading, blogs} = useBlogs();
+   const { loading, blogs } = useBlogs();
 
-   if(loading){ 
+   if (loading) {
       return <div>
-         <Appbar/>
+         <Appbar />
          <div className="flex justify-center">
             <div>
-               <BlogSkeleton/>
+               <BlogSkeleton />
             </div>
          </div>
       </div>
-   } 
- return <div>
-    <Appbar/>
+   }
+   return <div>
+      <Appbar />
       <div className="flex justify-center">
          <div>
-         {blogs.map(blog => <Blogcard
-         key={blog.id}
-         id={blog.id}
-         authorName={blog.authorName || "Anonymous"}
-         title={blog.title}
-         content={blog.content}
-         createdAt={"2nd Apr 2025"}
-         />)}
+            {blogs.map(blog => <Blogcard
+               key={blog.id}
+               id={blog.id}
+               authorName={blog.authorName || "Anonymous"}
+               title={blog.title}
+               content={blog.content}
+               createdAt={"2nd Apr 2025"}
+            />)}
          </div>
       </div>
-    </div>
+   </div>
 
 }
